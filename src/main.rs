39d6dc104694c3db8de995419b964e9d6d42c45b
Rémi Fabas <remi.fabas@easymile.com>
2023-04-12 15:38:54 +0200

    wip not working

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
+use log::*;
 use reqwest::{Client, Error};
 
 mod swgoh;
 use swgoh::Player;
-
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    println!("Hello, world!");
+    env_logger::init();
 
-    let p: Player = Client::new()
-        .get("http://api.swgoh.gg/player/327786519/")
-        .send()
-        .await?
-        .json()
-        .await?;
+    let ally_codes = vec![
+        // String::from("616247683"), // Phôenyx
+        String::from("327786519"), //Whills Wotan
+    ];
 
-    println!("{:#?}", p);
+    loopAlly(ally_codes).await;
+
+    match loopAlly(ally_codes).await {
+        Ok(_) => Ok("Done !"),
+        Err(e) => error!("An error ocurred: {}", e),
+    };
+
+    Ok(())
+}
+
+async fn loopAlly(ally_codes: Vec<String>) -> Result<(), Error> {
+    let call_url: String = String::from("http://api.swgoh.gg/player/");
+
+    for c in ally_codes {
+        let mut url = call_url.clone() + &c;
+        url.push('/');
+
+        let p: Player = Client::new().get(url).send().await?.json().await?;
+
+        println!("{:#?}", p);
+    }
 
     Ok(())
 }
--- src/swgoh/mod.rs
+++ src/swgoh/mod.rs
@@ -10,7 +10,7 @@ pub struct SWgohMod {}
 pub struct Player {
     #[serde(rename = "data")]
     pub datas: Data,
-    #[serde(rename = "units")]
+    //#[serde(rename = "units")]
     pub units: Vec<Unit>,
     //pub mods: Vec<swgohmod>,
     //pub datacrons: Vec<Datacron>,
@@ -26,6 +26,22 @@ pub struct Data {
     galactic_power: i32,
     character_galactic_power: i32,
     ship_galactic_power: i32,
+    ship_battles_won: i32,
+    pvp_battles_won: i32,
+    pve_battles_won: i32,
+    pve_hard_won: i32,
+    galactic_war_won: i32,
+    guild_raid_won: i32,
+    guild_contribution: i32,
+    guild_exchange_donations: i32,
+    season_full_clears: i32,
+    season_successful_defends: i32,
+    season_league_score: i32,
+    season_undersized_squad_wins: i32,
+    season_promotions_earned: i32,
+    season_banners_earned: i32,
+    season_offensive_battles_won: i32,
+    season_territories_defeated: i32,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -41,4 +57,36 @@ pub struct UnitData {
     level: i8,
     power: i32,
     rarity: i8,
+    combat_type: i8, // 1 char, 2 ship
+    relic_tier: i8,  // should be minus 2 for real value (r0 = 1, r1 = 2 ... )
+    stats: Stats,
+}
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Stats {
+    #[serde(rename = "1")]
+    health: f32,
+    #[serde(rename = "2")]
+    strength: f32,
+    #[serde(rename = "3")]
+    agility: f32,
+    #[serde(rename = "4")]
+    tactics: f32,
+    #[serde(rename = "5")]
+    speed: f32,
+    #[serde(rename = "6")]
+    physical_damage: f32,
+    #[serde(rename = "7")]
+    special_damage: f32,
+    #[serde(rename = "8")]
+    armor: f32,
+    #[serde(rename = "9")]
+    resistance: f32,
+    #[serde(rename = "10")]
+    armor_penetration: f32,
+    #[serde(rename = "11")]
+    resistance_penetration: f32,
+    #[serde(rename = "12")]
+    dodge_chance: f32,
+    #[serde(rename = "13")]
+    deflection_chance: f32,
 }

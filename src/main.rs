f7f93f8c7eec7c236a020b8d68f76de801bafc15
Rémi Fabas <remi.fabas@easymile.com>
2023-04-12 18:07:08 +0200

    feat: remove old code

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
 mod swgoh;
-use reqwest;
 use swgoh::Player;
-
-/*async fn loopAlly(ally_codes: Vec<String>) -> Result {
-    let call_url: String = String::from("http://api.swgoh.gg/player/");
-
-    for c in ally_codes {
-        let mut url = call_url.clone() + &c;
-        url.push('/');
-
-        let p: Player = Client::new().get(url).send().await?.json().await?;
-
-        println!("{:#?}", p);
-    }
-}*/
 
 async fn get_player_info(player_id: &str) -> Result<Player, reqwest::Error> {
     let url = format!("http://api.swgoh.gg/player/{}", player_id);
@@ -27,14 +13,17 @@ async fn get_player_info(player_id: &str) -> Result<Player, reqwest::Error> {
 #[tokio::main]
 async fn main() {
     let ally_codes = vec![
-        String::from("616247683"), // Phôenyx
-        String::from("327786519"), //Whills Wotan
+        String::from("616247683"), //   Phôenyx
+        String::from("327786519"), //   Whills Wotan
     ];
+
+    let mut players: Vec<swgoh::Player> = vec![];
 
     for ally_code in ally_codes {
         match get_player_info(&ally_code).await {
             Ok(p) => {
                 println!("{:#?}", p);
+                players.push(p);
             }
             Err(e) => {
                 eprintln!("Error: {}", e);

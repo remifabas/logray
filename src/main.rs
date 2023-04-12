6dad644ccb3304840e629f5be58875f64373a2fb
Rémi Fabas <remi.fabas@easymile.com>
2023-04-12 18:26:34 +0200

    feat: write to csv

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 mod swgoh;
+use csv::{Writer, WriterBuilder};
+use std::fs::File;
 use swgoh::Player;
 
 async fn get_player_info(player_id: &str) -> Result<Player, reqwest::Error> {
@@ -10,24 +12,39 @@ async fn get_player_info(player_id: &str) -> Result<Player, reqwest::Error> {
     Ok(player)
 }
 
+fn write_to_csv_file(player_id: String, p: swgoh::Player, writer: &mut Writer<File>) {
+    if let Err(e) = writer.write_record(&[player_id, p.datas.name]) {
+        eprintln!("Error writing to CSV file: {}", e);
+    }
+}
+
 #[tokio::main]
 async fn main() {
+    let mut writer = WriterBuilder::new()
+        .has_headers(false)
+        .from_path("output.csv")
+        .expect("Failed to create CSV writer");
+
     let ally_codes = vec![
         String::from("616247683"), //   Phôenyx
         String::from("327786519"), //   Whills Wotan
+        String::from("543168732"), //   M'enfin
     ];
 
-    let mut players: Vec<swgoh::Player> = vec![];
+    //let mut players: Vec<swgoh::Player> = vec![];
 
     for ally_code in ally_codes {
         match get_player_info(&ally_code).await {
             Ok(p) => {
                 println!("{:#?}", p);
-                players.push(p);
+                //players.push(p);
+                write_to_csv_file(ally_code, p, &mut writer);
             }
             Err(e) => {
                 eprintln!("Error: {}", e);
             }
         }
     }
+
+    writer.flush().expect("Failed to flush CSV writer");
 }
